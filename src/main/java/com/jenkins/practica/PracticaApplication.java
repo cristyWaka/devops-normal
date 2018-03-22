ed4ef8aa62da2f22ee0ae9bf5759fd3064178f5f
Isaac Alvarez <Isaac.Alvarez.Diz@norwegian.com>
2018-03-22 09:57:30 +0100

    Fix war deployment

--- src/main/java/com/jenkins/practica/PracticaApplication.java
+++ src/main/java/com/jenkins/practica/PracticaApplication.java
@@ -1,12 +1,18 @@
 package com.jenkins.practica;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PracticaApplication {
+public class PracticaApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PracticaApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PracticaApplication.class, args);
 	}
